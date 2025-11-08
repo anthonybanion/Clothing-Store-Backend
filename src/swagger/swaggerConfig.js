c47bcf1a829f7fdf1f16567bd79d6b3c45c8a8f8
anthonybanion <abanon@fi.uba.ar>
2025-11-08 14:24:19 -0300

    [Clothing-Store][backend]=>Add logger with Winston library

--- src/swagger/swaggerConfig.js
+++ src/swagger/swaggerConfig.js
@@ -6,9 +6,12 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Leer TODOS los archivos JSON
+// Read all swagger JSON files
 const swagger = JSON.parse(
   readFileSync(join(__dirname, 'swagger.json'), 'utf8')
+);
+const authSwagger = JSON.parse(
+  readFileSync(join(__dirname, 'authSwagger.json'), 'utf8')
 );
 const categorySwagger = JSON.parse(
   readFileSync(join(__dirname, 'categorySwagger.json'), 'utf8')
@@ -29,11 +32,12 @@ const accountSwagger = JSON.parse(
   readFileSync(join(__dirname, 'accountSwagger.json'), 'utf8')
 );
 
-// Combinar TODAS las entidades
+// Merge all swagger JSON files into a single specification
 const swaggerSpec = {
   ...swagger,
   paths: {
     ...swagger.paths,
+    ...authSwagger.paths,
     ...categorySwagger.paths,
     ...productSwagger.paths,
     ...orderSwagger.paths,
@@ -45,6 +49,7 @@ const swaggerSpec = {
     ...swagger.components,
     schemas: {
       ...swagger.components?.schemas,
+      ...authSwagger.components?.schemas,
       ...categorySwagger.components?.schemas,
       ...productSwagger.components?.schemas,
       ...orderSwagger.components?.schemas,
